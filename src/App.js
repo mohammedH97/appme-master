355968a954cc0562a0b29367d748c6524b9da084
mohammedH97 <102896991+mohammedH97@users.noreply.github.com>
2023-05-01 08:31:46 +0300

    brach mohammed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
    <div>
-     <h1>version 2</h1>
+     <h1>mohammed 2</h1>
      <h1>version 3</h1>
      <h1 className="name">Mohammed Ali</h1>
      <h1 className="name">Mohammed Ali</h1>
